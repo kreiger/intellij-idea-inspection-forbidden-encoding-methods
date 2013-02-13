5d74e3d7d9793a71b4cbb1f18872ea5f2305d72b
Christoffer Hammarström <kreiger@linuxgods.com>
2013-02-13 11:38:16 +0100

    Handle more methods.

--- src/main/java/com/linuxgods/kreiger/ForbiddenEncodingMethodsInspection.java
+++ src/main/java/com/linuxgods/kreiger/ForbiddenEncodingMethodsInspection.java
@@ -10,11 +10,21 @@ import com.intellij.psi.*;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.codeInspection.ProblemHighlightType.*;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class ForbiddenEncodingMethods extends BaseJavaLocalInspectionTool {
+import static com.intellij.codeInspection.ProblemHighlightType.*;
+import static com.intellij.psi.CommonClassNames.JAVA_LANG_STRING;
+
+public class ForbiddenEncodingMethodsInspection extends BaseJavaLocalInspectionTool {
 
     public static final AddEncodingParameter ADD_ENCODING_PARAMETER = new AddEncodingParameter();
+    public static final ForbiddenEncodingMethod[] FORBIDDEN_ENCODING_METHODS = new ForbiddenEncodingMethod[]{
+            new ForbiddenEncodingMethod(String.class, "getBytes"),
+            new ForbiddenEncodingMethod(String.class, "String", "byte[]"),
+            new ForbiddenEncodingMethod(InputStreamReader.class, "InputStreamReader", "InputStream"),
+            new ForbiddenEncodingMethod(OutputStreamWriter.class, "OutputStreamWriter", "OutputStream"),
+    };
 
     @NotNull
     @Override
@@ -34,26 +44,30 @@ public class ForbiddenEncodingMethods extends BaseJavaLocalInspectionTool {
         @Override
         public void visitCallExpression(final PsiCallExpression expression) {
             super.visitCallExpression(expression);
-            if (isCallTo(expression, CommonClassNames.JAVA_LANG_STRING, "getBytes")) {
-                holder.registerProblem(expression, "Method uses platform default encoding", GENERIC_ERROR_OR_WARNING, (TextRange)null, ADD_ENCODING_PARAMETER);
-            } else if (isCallTo(expression, CommonClassNames.JAVA_LANG_STRING, "String", "byte[]")) {
-                holder.registerProblem(expression, "Method uses platform default encoding", GENERIC_ERROR_OR_WARNING, (TextRange)null, ADD_ENCODING_PARAMETER);
+            for (ForbiddenEncodingMethod forbiddenEncodingMethod : FORBIDDEN_ENCODING_METHODS) {
+                if (isCallTo(expression, forbiddenEncodingMethod)) {
+                    holder.registerProblem(expression, "Method uses platform default encoding", GENERIC_ERROR_OR_WARNING, (TextRange) null, ADD_ENCODING_PARAMETER);
+                }
             }
         }
 
-        private static boolean isCallTo(PsiCall expression,
-                                        String className, @NonNls String methodName, String... parameters) {
+        private static boolean isCallTo(PsiCall expression, ForbiddenEncodingMethod forbiddenEncodingMethod) {
             PsiExpressionList argumentList = expression.getArgumentList();
             if (null == argumentList) {
                 return false;
             }
             PsiType[] argumentTypes = argumentList.getExpressionTypes();
-            if (argumentTypes.length != parameters.length) {
+            String[] parameterTypes = forbiddenEncodingMethod.getParameterTypes();
+            if (argumentTypes.length != parameterTypes.length) {
                 return false;
             }
             for (int i = 0; i < argumentTypes.length; i++) {
                 PsiType argumentType = argumentTypes[i];
-                if (!parameters[i].equals(argumentType.getCanonicalText())) {
+                String parameterType = parameterTypes[i];
+                if (null == argumentType) {
+                    return false;
+                }
+                if (!parameterType.equals(argumentType.getCanonicalText())) {
                     return false;
                 }
             }
@@ -63,10 +77,10 @@ public class ForbiddenEncodingMethods extends BaseJavaLocalInspectionTool {
             }
             @NonNls final String expressionMethodName =
                     method.getName();
-            if (!methodName.equals(expressionMethodName)) {
+            if (!forbiddenEncodingMethod.getMethodName().equals(expressionMethodName)) {
                 return false;
             }
-            return isMethodOfClass(expression.resolveMethod(), className);
+            return isMethodOfClass(expression.resolveMethod(), forbiddenEncodingMethod.getClassName());
         }
 
         private static boolean isMethodOfClass(PsiMethod method, String className) {
@@ -85,6 +99,7 @@ public class ForbiddenEncodingMethods extends BaseJavaLocalInspectionTool {
         }
 
     }
+
     private static class AddEncodingParameter implements LocalQuickFix {
 
         @NotNull
@@ -102,8 +117,41 @@ public class ForbiddenEncodingMethods extends BaseJavaLocalInspectionTool {
         @Override
         public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
             PsiCallExpression expression = (PsiCallExpression) descriptor.getPsiElement();
-            PsiExpression encodingString = JavaPsiFacade.getElementFactory(project).createExpressionFromText("Charset.forName(\"UTF-8\")", expression);
-            expression.getArgumentList().add(encodingString);
+            PsiExpressionList argumentList = expression.getArgumentList();
+            if (argumentList == null) {
+                return;
+            }
+            argumentList.add(JavaPsiFacade.getElementFactory(project).createExpressionFromText("java.nio.charset.Charset.forName(\"UTF-8\")", expression));
+        }
+    }
+
+    private static class ForbiddenEncodingMethod {
+        private final String className;
+        @NonNls
+        private final String methodName;
+        private final String[] parameterTypes;
+
+        private ForbiddenEncodingMethod(String className, @NonNls String methodName, String... parameterTypes) {
+            this.className = className;
+            this.methodName = methodName;
+            this.parameterTypes = parameterTypes;
+        }
+
+        public ForbiddenEncodingMethod(Class<?> aClass, String methodName, String... parameterTypes) {
+            this(aClass.getName(), methodName, parameterTypes);
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        @NonNls
+        public String getMethodName() {
+            return methodName;
+        }
+
+        public String[] getParameterTypes() {
+            return parameterTypes;
         }
     }
 }
