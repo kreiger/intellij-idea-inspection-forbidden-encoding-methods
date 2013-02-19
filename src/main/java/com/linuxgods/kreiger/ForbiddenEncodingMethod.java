8ae8be82a5c818584b09ee5603f2194431bf66a7
Christoffer Hammarström <kreiger@linuxgods.com>
2013-02-19 03:41:47 +0100

    Build method descriptions fluently.

---
+++ src/main/java/com/linuxgods/kreiger/ForbiddenEncodingMethod.java
@@ -0,0 +1,225 @@
+package com.linuxgods.kreiger;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.psi.*;
+import org.jetbrains.annotations.NonNls;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+class ForbiddenEncodingMethod {
+
+    private List<Condition> conditions = new LinkedList<Condition>();
+    private List<LocalQuickFix> fixes = new ArrayList<LocalQuickFix>();
+
+    private static boolean argumentsDiffer(PsiCall expression, Class[] parameterTypes) {
+        PsiExpressionList argumentList = expression.getArgumentList();
+        return null == argumentList || argumentsDiffer(argumentList, parameterTypes);
+    }
+
+    private static boolean argumentsDiffer(PsiExpressionList argumentList, Class[] parameterTypes) {
+        PsiType[] argumentTypes = argumentList.getExpressionTypes();
+        return argumentsDiffer(argumentTypes, parameterTypes);
+    }
+
+    private static boolean methodsDiffer(PsiMethod method, String methodName) {
+        if (null == method) {
+            return true;
+        }
+        @NonNls final String expressionMethodName =
+                method.getName();
+        return !methodName.equals(expressionMethodName);
+    }
+
+    private static boolean argumentsDiffer(PsiType[] argumentTypes, Class[] parameterTypes) {
+        if (argumentTypes.length != parameterTypes.length) {
+            return true;
+        }
+        for (int i = 0; i < argumentTypes.length; i++) {
+            PsiType argumentType = argumentTypes[i];
+            Class parameterType = parameterTypes[i];
+            if (null == argumentType) {
+                return true;
+            }
+            if (!isSubClassOrSameOf(argumentType, parameterType)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isSubClassOrSameOf(PsiType argumentType, Class parameterType) {
+        if (argumentType.getCanonicalText().equals(parameterType.getCanonicalName())) {
+            return true;
+        }
+
+        for (PsiType type : argumentType.getSuperTypes()) {
+            if (type.getCanonicalText().equals(parameterType.getCanonicalName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isMethodOfClass(PsiCallExpression expression, Class<?> clazz) {
+        if (expression instanceof PsiMethodCallExpression) {
+            PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) expression;
+            PsiExpression qualifierExpression = methodCallExpression.getMethodExpression().getQualifierExpression();
+            if (qualifierExpression == null) {
+                return false;
+            }
+            PsiType type = qualifierExpression.getType();
+            return type != null && clazz.getCanonicalName().equals(type.getCanonicalText());
+        }
+        PsiMethod method = expression.resolveMethod();
+        if (method == null) {
+            return false;
+        }
+        final PsiClass aClass = method.getContainingClass();
+        if (aClass == null) {
+            return false;
+        }
+        final String expressionClassName = aClass.getQualifiedName();
+        return expressionClassName != null && clazz.getCanonicalName().equals(expressionClassName);
+
+    }
+
+
+    public boolean matches(PsiCallExpression expression) {
+        for (Condition condition : conditions) {
+            if (!condition.matches(expression)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public LocalQuickFix[] getFixes() {
+        return fixes.toArray(new LocalQuickFix[fixes.size()]);
+    }
+
+    public static ForbiddenEncodingMethod methodsOf(final Class<?> clazz) {
+        return new ForbiddenEncodingMethod().addCondition(new ClassCondition(clazz));
+    }
+
+    public ForbiddenEncodingMethod named(final String methodName) {
+        return addCondition(new NameCondition(methodName));
+    }
+
+    public static ForbiddenEncodingMethod constructorsOf(final Class<?> clazz) {
+        return methodsOf(clazz).addCondition(new ConstructorCondition());
+    }
+
+    private ForbiddenEncodingMethod addCondition(Condition condition) {
+        conditions.add(condition);
+        return this;
+    }
+
+    public ForbiddenEncodingMethod withParameterTypes(final Class... parameterTypes) {
+        return addCondition(new ParameterTypesCondition(parameterTypes));
+        
+    }
+
+    public ForbiddenEncodingMethod whichDontThrow(final Class<? extends Throwable> throwableClass) {
+        return addCondition(new Condition() {
+            @Override
+            public boolean matches(PsiCallExpression expression) {
+                PsiMethod method = expression.resolveMethod();
+                if (null == method) {
+                    return false;
+                }
+                PsiReferenceList throwsList = method.getThrowsList();
+                PsiClassType[] referencedTypes = throwsList.getReferencedTypes();
+                for (PsiClassType referencedType : referencedTypes) {
+                    if (referencedType.getClassName().equals(throwableClass.getName())) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    public ForbiddenEncodingMethod withoutParameters() {
+        return withParameterTypes();
+    }
+
+    public ForbiddenEncodingMethod hasFix(LocalQuickFix fix) {
+        this.fixes.add(fix);
+        return this;
+    }
+
+
+    public static interface Condition {
+
+        public boolean matches(PsiCallExpression expression);
+
+    }
+
+    private static class ConstructorCondition implements Condition {
+        @Override
+        public boolean matches(PsiCallExpression expression) {
+            PsiMethod method = expression.resolveMethod();
+            return null != method && method.isConstructor();
+        }
+    }
+
+    private static class ParameterTypesCondition implements Condition {
+        private final Class[] parameterTypes;
+
+        public ParameterTypesCondition(Class... parameterTypes) {
+            this.parameterTypes = parameterTypes;
+        }
+
+        @Override
+        public boolean matches(PsiCallExpression expression) {
+            return !argumentsDiffer(expression, parameterTypes);
+        }
+        @Override
+
+        public String toString() {
+            return super.toString()+":"+asList(parameterTypes);
+        }
+
+    }
+
+    private static class NameCondition implements Condition {
+        private final String methodName;
+
+        public NameCondition(String methodName) {
+            this.methodName = methodName;
+        }
+
+        @Override
+        public boolean matches(PsiCallExpression expression) {
+            return !methodsDiffer(expression.resolveMethod(), methodName);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString()+":"+methodName;
+        }
+    }
+
+    private static class ClassCondition implements Condition {
+        private final Class<?> clazz;
+
+        public ClassCondition(Class<?> clazz) {
+            this.clazz = clazz;
+        }
+
+        @Override
+        public boolean matches(PsiCallExpression expression) {
+            return isMethodOfClass(expression, clazz);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString()+":"+clazz.getName();
+        }
+
+    }
+}
--- src/main/java/com/linuxgods/kreiger/ForbiddenEncodingMethodsInspection.java
+++ src/main/java/com/linuxgods/kreiger/ForbiddenEncodingMethodsInspection.java
@@ -5,102 +5,65 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.nio.charset.Charset;
 
 import static com.intellij.codeInspection.ProblemHighlightType.*;
-import static com.intellij.psi.CommonClassNames.JAVA_LANG_STRING;
+import static com.linuxgods.kreiger.ForbiddenEncodingMethod.methodsOf;
+import static com.linuxgods.kreiger.ForbiddenEncodingMethod.constructorsOf;
 
 public class ForbiddenEncodingMethodsInspection extends BaseJavaLocalInspectionTool {
 
-    public static final AddEncodingParameter ADD_ENCODING_PARAMETER = new AddEncodingParameter();
+    public static final AddEncodingParameter ADD_STRING = new AddEncodingParameter("\"UTF-8\"");
+    public static final AddEncodingParameter ADD_CHARSET = new AddEncodingParameter("Charset.forName(\"UTF-8\")");
+
     public static final ForbiddenEncodingMethod[] FORBIDDEN_ENCODING_METHODS = new ForbiddenEncodingMethod[]{
-            new ForbiddenEncodingMethod(String.class, "getBytes"),
-            new ForbiddenEncodingMethod(String.class, "String", "byte[]"),
-            new ForbiddenEncodingMethod(InputStreamReader.class, "InputStreamReader", "InputStream"),
-            new ForbiddenEncodingMethod(OutputStreamWriter.class, "OutputStreamWriter", "OutputStream"),
+            methodsOf(String.class).named("getBytes").withoutParameters().hasFix(ADD_CHARSET),
+            constructorsOf(String.class).withParameterTypes(byte[].class).hasFix(ADD_CHARSET),
+            methodsOf(Charset.class).named("defaultCharset"),
+            methodsOf(FileWriter.class),
+            constructorsOf(InputStreamReader.class).withParameterTypes(InputStream.class).hasFix(ADD_CHARSET),
+            constructorsOf(OutputStreamWriter.class).withParameterTypes(OutputStream.class).hasFix(ADD_CHARSET),
+            constructorsOf(PrintStream.class).whichDontThrow(UnsupportedEncodingException.class).hasFix(ADD_STRING),
+            constructorsOf(PrintWriter.class).whichDontThrow(UnsupportedEncodingException.class).hasFix(ADD_STRING),
+
     };
 
     @NotNull
     @Override
     public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
-        return new ForbiddenEncodingMethodsVisitor(holder, isOnTheFly);
+        return new ForbiddenEncodingMethodsVisitor(holder);
     }
 
     private static class ForbiddenEncodingMethodsVisitor extends JavaElementVisitor {
         private ProblemsHolder holder;
-        private boolean onTheFly;
 
-        public ForbiddenEncodingMethodsVisitor(ProblemsHolder holder, boolean onTheFly) {
+        public ForbiddenEncodingMethodsVisitor(ProblemsHolder holder) {
             this.holder = holder;
-            this.onTheFly = onTheFly;
         }
 
         @Override
         public void visitCallExpression(final PsiCallExpression expression) {
             super.visitCallExpression(expression);
             for (ForbiddenEncodingMethod forbiddenEncodingMethod : FORBIDDEN_ENCODING_METHODS) {
-                if (isCallTo(expression, forbiddenEncodingMethod)) {
-                    holder.registerProblem(expression, "Method uses platform default encoding", GENERIC_ERROR_OR_WARNING, (TextRange) null, ADD_ENCODING_PARAMETER);
+                if (forbiddenEncodingMethod.matches(expression)) {
+                    holder.registerProblem(expression, "Method uses platform default encoding", GENERIC_ERROR_OR_WARNING, forbiddenEncodingMethod.getFixes());
                 }
             }
-        }
-
-        private static boolean isCallTo(PsiCall expression, ForbiddenEncodingMethod forbiddenEncodingMethod) {
-            PsiExpressionList argumentList = expression.getArgumentList();
-            if (null == argumentList) {
-                return false;
-            }
-            PsiType[] argumentTypes = argumentList.getExpressionTypes();
-            String[] parameterTypes = forbiddenEncodingMethod.getParameterTypes();
-            if (argumentTypes.length != parameterTypes.length) {
-                return false;
-            }
-            for (int i = 0; i < argumentTypes.length; i++) {
-                PsiType argumentType = argumentTypes[i];
-                String parameterType = parameterTypes[i];
-                if (null == argumentType) {
-                    return false;
-                }
-                if (!parameterType.equals(argumentType.getCanonicalText())) {
-                    return false;
-                }
-            }
-            PsiMethod method = expression.resolveMethod();
-            if (null == method) {
-                return false;
-            }
-            @NonNls final String expressionMethodName =
-                    method.getName();
-            if (!forbiddenEncodingMethod.getMethodName().equals(expressionMethodName)) {
-                return false;
-            }
-            return isMethodOfClass(expression.resolveMethod(), forbiddenEncodingMethod.getClassName());
-        }
-
-        private static boolean isMethodOfClass(PsiMethod method, String className) {
-            if (method == null) {
-                return false;
-            }
-            final PsiClass aClass = method.getContainingClass();
-            if (aClass == null) {
-                return false;
-            }
-            final String expressionClassName = aClass.getQualifiedName();
-            if (expressionClassName == null) {
-                return false;
-            }
-            return className.equals(expressionClassName);
         }
 
     }
 
     private static class AddEncodingParameter implements LocalQuickFix {
+
+        private String text;
+
+        private AddEncodingParameter(String text) {
+            this.text = text;
+        }
 
         @NotNull
         @Override
@@ -121,37 +84,8 @@ public class ForbiddenEncodingMethodsInspection extends BaseJavaLocalInspectionT
             if (argumentList == null) {
                 return;
             }
-            argumentList.add(JavaPsiFacade.getElementFactory(project).createExpressionFromText("java.nio.charset.Charset.forName(\"UTF-8\")", expression));
+            argumentList.add(JavaPsiFacade.getElementFactory(project).createExpressionFromText(text, expression));
         }
     }
 
-    private static class ForbiddenEncodingMethod {
-        private final String className;
-        @NonNls
-        private final String methodName;
-        private final String[] parameterTypes;
-
-        private ForbiddenEncodingMethod(String className, @NonNls String methodName, String... parameterTypes) {
-            this.className = className;
-            this.methodName = methodName;
-            this.parameterTypes = parameterTypes;
-        }
-
-        public ForbiddenEncodingMethod(Class<?> aClass, String methodName, String... parameterTypes) {
-            this(aClass.getName(), methodName, parameterTypes);
-        }
-
-        public String getClassName() {
-            return className;
-        }
-
-        @NonNls
-        public String getMethodName() {
-            return methodName;
-        }
-
-        public String[] getParameterTypes() {
-            return parameterTypes;
-        }
-    }
 }
